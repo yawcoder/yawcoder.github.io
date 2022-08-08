911801a5bb9f4f776cd994ae0b5014eb919f27e8
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2022-08-08 18:02:54 +0000

    almost

--- app.js
+++ app.js
@@ -68,24 +68,39 @@ const projects = [
 const project1 = document.querySelector('.project-1');
 const project2 = document.querySelector('.project-2');
 const project3 = document.querySelector('.project-3');
+const project4 = document.querySelector('.project-4');
+const project5 = document.querySelector('.project-5');
+const project6 = document.querySelector('.project-6');
 
 window.addEventListener("DOMContentLoaded", () => {
     let homePageProjects = projects.filter((item) => {
-        return item.id > projects.length-3;
+        return item.id > projects.length-6;
     });
-    // console.log(homePageProjects);
-    project1.innerHTML = `<div class="card"><a href=${homePageProjects[2].address} target="_blank">
-                     <img src=${homePageProjects[2].img} alt=${homePageProjects[2].title} class="project-img mb-3">
-                     <h5 class="project-title">${homePageProjects[2].title}</h5>
+     //console.log(homePageProjects);
+    project1.innerHTML = `<div class="card"><a href=${homePageProjects[5].address} target="_blank">
+                     <img src=${homePageProjects[5].img} alt=${homePageProjects[5].title} class="project-img mb-3">
+                     <h5 class="project-title">${homePageProjects[5].title}</h5>
                  </a></div>`;
-    project2.innerHTML = `<div class="card"><a href=${homePageProjects[1].address} target="_blank">
-                     <img src=${homePageProjects[1].img} alt=${homePageProjects[1].title} class="project-img mb-3">
-                     <h5 class="project-title">${homePageProjects[1].title}</h5>
+    project2.innerHTML = `<div class="card"><a href=${homePageProjects[4].address} target="_blank">
+                     <img src=${homePageProjects[4].img} alt=${homePageProjects[4].title} class="project-img mb-3">
+                     <h5 class="project-title">${homePageProjects[4].title}</h5>
                  </a></div>`;
-    project3.innerHTML = `<div class="card"><a href=${homePageProjects[0].address} target="_blank">
-                     <img src=${homePageProjects[0].img} alt=${homePageProjects[0].title} class="project-img mb-3">
-                     <h5 class="project-title">${homePageProjects[0].title}</h5>
+    project3.innerHTML = `<div class="card"><a href=${homePageProjects[3].address} target="_blank">
+                     <img src=${homePageProjects[3].img} alt=${homePageProjects[3].title} class="project-img mb-3">
+                     <h5 class="project-title">${homePageProjects[3].title}</h5>
                  </a></div>`;
+    project4.innerHTML = `<div class="card"><a href=${homePageProjects[2].address} target="_blank">
+                 <img src=${homePageProjects[2].img} alt=${homePageProjects[2].title} class="project-img mb-3">
+                 <h5 class="project-title">${homePageProjects[2].title}</h5>
+             </a></div>`;
+    project5.innerHTML = `<div class="card"><a href=${homePageProjects[1].address} target="_blank">
+                 <img src=${homePageProjects[1].img} alt=${homePageProjects[1].title} class="project-img mb-3">
+                 <h5 class="project-title">${homePageProjects[1].title}</h5>
+             </a></div>`;
+    project6.innerHTML = `<div class="card"><a href=${homePageProjects[0].address} target="_blank">
+                 <img src=${homePageProjects[0].img} alt=${homePageProjects[0].title} class="project-img mb-3">
+                 <h5 class="project-title">${homePageProjects[0].title}</h5>
+             </a></div>`;
 
 });
 
